1264b8b43d4816a9ffcb58ca1e0f01562b2dae81
Andrew-1981 <andrey.dobryakov.1981@mail.ru>
2022-01-28 15:03:49 +0300

    правила работы с функциями

--- js1.js
+++ js1.js
@@ -1,91 +1,95 @@
-// // ФУНКЦИИ
+// ФУНКЦИИ
 
-// console.log(window)
-// console.log(window.innerHeight)
-// console.log(window.innerWidth)
-// console.log(screen.height)
-// console.log(screen.width)
+console.log(window)
+console.log(window.innerHeight)
+console.log(window.innerWidth)
+console.log(screen.height)
+console.log(screen.width)
 
-// // простая функция
+// простая функция
 
-// let a = 5,
-//   b = 6
+let a = 5,
+  b = 6
 
-// function sum(d, e) {
-//   const c = d + e
-//   console.log(c)
-// }
+function sum(d, e) {
+  const c = d + e
+  console.log(c)
+}
 
-// sum(a, b)
+sum(a, b)
 
-// // функция- это объект, который имеет свои свойства
+// функция- это объект, который имеет свои свойства
 
-// function great(a, b, c, d) {
-//   const f = a + b + c + d
-// }
+function great(a, b, c, d) {
+  const f = a + b + c + d
+}
 
-// great()
+great()
 
-// console.dir(great)
-// console.log(great)
+console.dir(great)
+console.log(great)
 
-// function dpr() {
-//   return window.devicePixelRatio
-// }
-// console.log(dpr())
+function dpr() {
+  return window.devicePixelRatio
+}
+console.log(dpr())
 
-// nord(3, 5);
+nord(3, 5);
 
-// function nord(aa = 7, bb = 8, cc = 9) {
-//   console.log(aa, bb, cc)
-// }
+function nord(aa = 7, bb = 8, cc = 9) {
+  console.log(aa, bb, cc)
+}
 
-// // не рекомендуется мутировать объекты внутри функции, аргумент объект передает ссылку в качестве параметра
+// не рекомендуется мутировать объекты внутри функции, аргумент объект передает ссылку в качестве параметра
 
-// const green = {
-//   abc: 100
-// }
+const green = {
+  abc: 100
+}
 
-// function def(red) {
-//   red.abc += 5
-// }
+function def(red) {
+  red.abc += 5
+}
 
-// def(green)
+def(green)
 
-// console.log(green.abc)
+console.log(green.abc)
 
-// // лучше использовать копирование объекта
+// лучше использовать копирование объекта
 
-// const ch = {
-//   ab: 1999
-// }
+const ch = {
+  ab: 1999
+}
 
-// function cd(pr) {
-//   const fff = Object.assign({}, pr)
-//   fff.ab += 1
-//   return fff
-// }
+function cd(pr) {
+  const fff = Object.assign({}, pr)
+  fff.ab += 1
+  return fff
+}
 
-// const dd = cd(ch)
+// если нет команды return функция по умолчанию возвращает undefined
 
-// console.log(ch.ab)
-// console.log(dd.ab)
+const dd = cd(ch)
 
-// // КОЛБЭК ФУНКЦИИ
+console.log(ch.ab)
+console.log(dd.ab)
 
-// // не работает хзчт
-// function ik() {
-//   const d = 10
-//   console.log(d)
-// }
 
-// function ww(tt) {
-//   function tt() {}
-// }
 
-// ww(ik)
+// КОЛБЭК ФУНКЦИИ
 
-// // Пример колбэк функции
+// не работает хзчт
+function ik() {
+  const d = 10
+  console.log(d)
+}
+
+function ww(tt) {
+  function tt() {}
+}
+
+ww(ik)
+
+// Пример колбэк функции
 
 function wq() {
   console.log('Andrew')
@@ -94,4 +98,11 @@ function wq() {
 console.log('start')
 
 setTimeout(wq, 3000)
+
+// ПРАВИЛА РАБОТЫ С ФУНКЦИЯМИ
+
+// 1 Называть функцию глаголом, согласно выполняемых ею действий
+// 2 Одна функция должна выполнять одно действие
+// 3 Функция должна быть чистой, т.е. не изменять свойства внешних переменных, а работать только с внутренними
+
 
